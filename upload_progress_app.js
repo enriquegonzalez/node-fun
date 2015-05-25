6b6cd69f3bfc5b4b801c168bbf2105b4c912c6ea
Enrique <egonzalez78@gmail.com>
2015-05-24 22:15:47 -0700

    uploading files with a progress indicator

---
+++ upload_progress_app.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var http = require('http');
+
+http.createServer(function(request, response){
+  var newFile = fs.createWriteStream('readme_copy.md');
+  var fileBytes = request.headers['content-length'];
+  var uploadedBytes = 0;
+
+  request.on('readable', function(){
+    var chunk = null;
+    while(null !== (chunk = request.read())){
+      uploadedBytes += chunk.length;
+      var progress = (uploadedBytes / fileBytes) * 100;
+      response.write("progress: " + parseInt(progress, 10) + "%\n");
+    }
+  });
+
+  request.pipe(newFile);
+
+  request.on('end', function(){
+    response.end('Uploaded!');
+  });
+}).listen(8080);
+
+console.log("Listening on port 8080. . .");
